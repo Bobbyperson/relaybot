a4a4f2a09f76ea842428e958850bfe6107cb48b5
Bobbyperson12 <bobbyperson12345@gmail.com>
2024-08-21 05:29:16 -0400

    cleanup config

--- example.config.py
+++ example.config.py
@@ -1,8 +1,15 @@
 from server import Server
 
 TOKEN = ""
-ban_words = [r"f([aeiou*x])\1*g+([aeiou*x])\1*t", "nig+er"]  # automatic ban words
-bad_words = ["nigg", "fag", "bundle of sticks", "tnd"]  # admin warnings
+
+ban_words = ["fa+got", "nig+er"]  # automatic ban words
+bad_words = [
+    "nigg",
+    "fag",
+    "bundle of sticks",
+    "tnd",
+    r"f([aeiou*x])\1*g+([aeiou*x])\1*t",
+]  # admin warnings
 
 servers = [
     Server("server1", 0, "127.0.0.1", "secretkey1", "rconpass1"),
@@ -15,7 +22,6 @@ ban_log = 0
 channel_id = 0  # dedicated stats channel id
 query = ["server name"]
 masterurl = "https://northstar.tf/client/servers"  # don't change this
-whitelist_folder = "C:\Program Files (x86)\Steam\steamapps\common\Titanfall2\R2Northstar\save_data\Whitelist"
 debug = False
 log_channel = 0
 bigbrother = 0
